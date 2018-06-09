d4e0168c93a04b73d4aa208b5dbe3ae0e77bffe6
Johannes Filter <hi@jfilter.de>
2018-06-09 23:10:31 +0200

    add arg for the twitter handle

--- get_tweets.py
+++ get_tweets.py
@@ -1,8 +1,16 @@
+import argparse
+
 import twint
+
+parser = argparse.ArgumentParser()
+parser.add_argument("name")
+args = parser.parse_args()
+
+print(args.name)
 
 # Configure
 c = twint.Config()
-c.Username = "fil_ter"
+c.Username = args.name
 c.Store_csv = True
 c.Output = 'temp.csv'
 
