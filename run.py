689842bd90627b178144e2221f05ea44ecc6aa2e
Johannes Filter <hi@jfilter.de>
2018-06-30 12:37:11 +0200

    filter tweets by tweet id

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import tweets_with_images
 
-tweets_with_images.get_tweets('fil_ter', './data')
+tweets_with_images.get_tweets('fil_ter', './data', 608045345201881088)
 tweets_with_images.get_images('./data')
--- tweets_with_images/get_tweets.py
+++ tweets_with_images/get_tweets.py
@@ -1,12 +1,40 @@
+import csv
+import os
+
 import twint
 
 
-def get_tweets(name, path):
-    # Configure
+def _filter_out_previous_tweets(full_path, previous_tweet_id):
+    rows = []
+    fieldnames = None
+
+    with open(full_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        fieldnames = reader.fieldnames
+
+        for row in reader:
+            if int(row['id']) == previous_tweet_id:
+                print('match!')
+                break
+            else:
+                rows.append(row)
+
+    with open(full_path, newline='', mode='w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
+
+
+def get_tweets(name, path, previous_tweet_id=None):
+    full_path = os.path.join(path, 'temp.csv')
+
     c = twint.Config()
     c.Username = name
     c.Store_csv = True
-    c.Output = path + '/temp.csv'
+    c.Output = full_path
 
     # Run
     twint.run.Search(c)
+
+    if previous_tweet_id is not None:
+        _filter_out_previous_tweets(full_path, previous_tweet_id)
